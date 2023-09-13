22add344288f50c9d2dfbf89f77cdecda95652aa
molki <molman3@hotmail.com>
2023-09-13 12:03:59 -0600

    added images and better design for the main

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,8 @@ const router = Router();
 //cuando soliciten la siguiente ruta...
 router.get('/', (req, res) => res.render('index', {title: 'Main page'}))
 
-router.get('/contact', (req, res) => res.render('contact'))
+router.get('/contact', (req, res) => res.render('contact', {title: 'Contact'}))
 
-router.get('/about', (req, res) => res.render('about'))
+router.get('/about', (req, res) => res.render('about', {title: 'About'}))
 
 export default router;
